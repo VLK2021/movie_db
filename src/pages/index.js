778b7b4f8cc9a0771c974630ca2793001f9a6f10
vlk2022 <volodymyrkostiuk2021@gmail.com>
2022-08-18 11:48:43 +0300

    created index for all

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export * from './GenresFilmsList/GenresFilmsList';
+export * from './MovieDetails/MovieDetails';
+export * from './MovieGenres/MovieGenres';
+export * from './MoviesList/MoviesList';
+export * from './NewMovies/NewMovies';
+export * from './NotFoundPage/NotFoundPage';
+export * from './SearchFilms/SearchFilms';
